b853d2a227c44ee6582b702653c395bd589c2f67
Zone <enzo.gomez@3wa.io>
2024-03-20 16:18:10 +0100

    add features js

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,17 @@ menuBurger.addEventListener('click' , function(){
         navBar.style.display = "block"
     }
 })
+
+let btn = document.getElementById('btn')
+let email = document.getElementById('email').value
+let  regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; 
+let errorMsg = document.getElementById('error-msg')
+
+btn.addEventListener('click' , function(e){
+    e.preventDefault()
+    if(regexEmail.test(email)){
+        errorMsg.innerHTML = ` <span class="msg-good"> Vous êtes bien inscrit a notre News Letter </span>`;
+    }else {
+        errorMsg.innerHTML = `<span class="msg-error"> Entrer une adresse mail valide </span>` ;
+    }
+})
